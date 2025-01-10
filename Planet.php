f0a998f74c00fdc1e06e9ea200ac0e053efdecb9
Apocalypsecoder0 <Stephend8846@gmail.com>
2025-01-10 01:55:58 -0500

    Update Planet.php
    Signed-off-by: Apocalypsecoder0 <Stephend8846@gmail.com>

--- Planet.php
+++ Planet.php
@@ -1,7 +1,10 @@
 <?php
+
 class Planet {
     private $id;
     private $name;
+    private $description;
+    private $temperature;
     private $resources;
 
     public function __construct($id) {
@@ -10,13 +13,14 @@ class Planet {
     }
 
     private function loadPlanetData() {
-        // Load planet data from the database
         $pdo = new PDO('mysql:host=localhost;dbname=ogame', 'root', '');
         $stmt = $pdo->prepare("SELECT * FROM planets WHERE id = ?");
         $stmt->execute([$this->id]);
         $planet = $stmt->fetch();
 
         $this->name = $planet['name'];
+        $this->description = $planet['description'];
+        $this->temperature = $planet['temperature'];
         $this->resources = [
             'metal' => $planet['metal'],
             'crystal' => $planet['crystal'],
@@ -28,18 +32,37 @@ class Planet {
         return $this->name;
     }
 
+    public function getDescription() {
+        return $this->description;
+    }
+
+    public function getTemperature() {
+        return $this->temperature;
+    }
+
     public function getResources() {
         return $this->resources;
     }
 
     public function setResources($resources) {
         $this->resources = $resources;
-        // Update resources in the database
         $pdo = new PDO('mysql:host=localhost;dbname=ogame', 'root', '');
         $stmt = $pdo->prepare("UPDATE planets SET metal = ?, crystal = ?, deuterium = ? WHERE id = ?");
         $stmt->execute([$resources['metal'], $resources['crystal'], $resources['deuterium'], $this->id]);
     }
 
-    // Additional methods for planet actions can be added here
+    public function setDescription($description) {
+        $this->description = $description;
+        $pdo = new PDO('mysql:host=localhost;dbname=ogame', 'root', '');
+        $stmt = $pdo->prepare("UPDATE planets SET description = ? WHERE id = ?");
+        $stmt->execute([$description, $this->id]);
+    }
+
+    public function setTemperature($temperature) {
+        $this->temperature = $temperature;
+        $pdo = new PDO('mysql:host=localhost;dbname=ogame', 'root', '');
+        $stmt = $pdo->prepare("UPDATE planets SET temperature = ? WHERE id = ?");
+        $stmt->execute([$temperature, $this->id]);
+    }
 }
 ?>
